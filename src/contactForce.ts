ccfc8a9b27b32f904dbb2236ff61fb6d79a5f4cc
P-Miha <mplisko@students.uni-mainz.de>
2023-07-06 13:27:38 +0200

    WIP contactForce mathe teil

--- src/contactForce.ts
+++ src/contactForce.ts
@@ -1,6 +1,22 @@
 import { Matrix, Mesh, Vector3, Vector4, float } from "@babylonjs/core";
 import { SDFData } from "./sdfParser";
 import { index } from "./nagelDistanceField";
+/**
+ * Liefert das Ergebnis von einer Multiplikation von einer 3x3 Matrix und einem Vector3 (Matrix * Vector3)
+ * Als Funktion implementiert, da Babylon nur Punkttransformationen aber nicht basic Matrix berechnung hat
+ * @param matrix 
+ * @param vector 
+ * @returns vector3
+ */
+function multiplyMatrix3x3WithVector3(matrix: Matrix, vector: Vector3): Vector3 {
+  const result = new Vector3();
+
+  result.x = matrix.m[0] * vector.x + matrix.m[1] * vector.y + matrix.m[2] * vector.z;
+  result.y = matrix.m[4] * vector.x + matrix.m[5] * vector.y + matrix.m[6] * vector.z;
+  result.z = matrix.m[8] * vector.x + matrix.m[9] * vector.y + matrix.m[10] * vector.z;
+
+  return result;
+}
 
 /**
  * Berechnet die benötigte änderung in Position und Orientierung, um von einer überschneideten Kollision zu einer an der Oberfläche berührenden Kollision zu kommen.
@@ -49,3 +65,16 @@ function lambda(movingMesh: Mesh, contactPoint: Vector3, normal: Vector3, distan
     return lamda
 }
 
+function cDelta(movingMesh: Mesh, contactPoint: Vector3, normal: Vector3, distance: float, radius: float, mass: float) : Vector3{
+    const factor = ((1/mass)*lambda(movingMesh, contactPoint, normal, distance, radius, mass))
+    return normal.multiply(new Vector3(factor, factor, factor))
+}
+
+function qDelta(movingMesh: Mesh, contactPoint: Vector3, normal: Vector3, distance: float, radius: float, mass: float){
+const lambaTimesInverse = invertedIneratiaMatrix(radius, mass).scale(lambda(movingMesh, contactPoint, normal, distance, radius, mass))
+const radiusCrossNormal = contactPoint.cross(normal)
+const quaterionChange = multiplyMatrix3x3WithVector3(lambaTimesInverse, radiusCrossNormal).scale(1/2)
+
+return (0, quaterionChange) 
+
+}
