f7ac3d988d8ac9d9f2479298f25038b157ef7748
P-Miha <mplisko@students.uni-mainz.de>
2023-07-17 13:24:16 +0200

    First WebXR Implentation, need orientation fix. Added buildversion for Github-pages

--- src/scenes/nagelPuzzle.ts
+++ src/scenes/nagelPuzzle.ts
@@ -5,7 +5,7 @@ import { Matrix, Quaternion, Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { CreateGround } from "@babylonjs/core/Meshes/Builders/groundBuilder";
 import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
 import { CreateSceneClass } from "../createScene";
-import { BoundingInfo, Color3, HighlightLayer, Mesh, MeshBuilder, SceneLoader } from "@babylonjs/core";
+import { BoundingInfo, Color3, HighlightLayer, Mesh, MeshBuilder, Nullable, SceneLoader, WebXRControllerComponent } from "@babylonjs/core";
 // If you don't need the standard material you will still need to import it since the scene requires it.
 // import "@babylonjs/core/Materials/standardMaterial";
 import { Texture } from "@babylonjs/core/Materials/Textures/texture";
@@ -95,10 +95,16 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
             punkt.parent = nagelPunkte;
             punkt.visibility = 0;
         }
-        nagelPuzzleMoveable.position = new Vector3(-80, -10, 0);
+        nagelPuzzleMoveable.position = new Vector3(-0.5, 1, 0);
+        nagelPuzzleStatic.position = new Vector3(0, 1, 0);
 
         // Drehe Moveable Mesh um 180°
         nagelPuzzleMoveable.rotationQuaternion = Quaternion.FromEulerVector( new Vector3(Math.PI / 2, Math.PI, 0));
+
+        // Scaling der Meshe für WebXr
+        nagelPuzzleMoveable.scaling = new Vector3(0.01, 0.01, 0.01);
+        nagelPuzzleStatic.scaling = new Vector3(0.01, 0.01, 0.01);
+
   
         // Die URL der SDF-Datei
         const sdfFileUrl = 'https://raw.githubusercontent.com/P-Miha/Kyros-Zylinder/master/assets/SDFInformation/Nagel1.sdf';
@@ -107,8 +113,8 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
         const loadFile = loadSDFFile(sdfFileUrl);
         const sdfContent = parseSDFFileContent(await loadFile);
   
-        //Print SDF-Data
-        console.log("SDF-Data: ", sdfContent);
+        // //Print SDF-Data
+        // console.log("SDF-Data: ", sdfContent);
         
         // Erstelle empty mesh um eine Custom boundingbox zu erstellen
         const boundingBox = new Mesh("boundingBox", scene);
@@ -199,27 +205,103 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
         // );
        
         // DEBUG 
-        const sphere = MeshBuilder.CreateSphere(
-            "sphere",
-            { diameter: 1 },
-            scene
-        );
-        sphere.position = moveableNagelPunkte[0].absolutePosition;
-        const hl = new HighlightLayer("hl1", scene);
-        hl.addMesh(sphere, Color3.Green());
+        // const sphere = MeshBuilder.CreateSphere(
+        //     "sphere",
+        //     { diameter: 1 },
+        //     scene
+        // );
+        // sphere.position = moveableNagelPunkte[0].absolutePosition;
+        // const hl = new HighlightLayer("hl1", scene);
+        // hl.addMesh(sphere, Color3.Green());
         // Erstelle Pfeil
-        const direction = new Vector3(0, 0, 1);
-        const arrow = MeshBuilder.CreateTube('arrow', {
-            path: [new Vector3(0, 0, 0), direction.scale(2)],
-            radius: 0.05,
-            tessellation: 16,
-            updatable: true
-          }, scene);        
-        // Checke Punkte
-        scene.onAfterRenderObservable.add(() => { 
+        // const direction = new Vector3(0, 0, 1);
+        // const arrow = MeshBuilder.CreateTube('arrow', {
+        //     path: [new Vector3(0, 0, 0), direction.scale(2)],
+        //     radius: 0.05,
+        //     tessellation: 16,
+        //     updatable: true
+        //   }, scene);      
+          
+        // VR-Integration, "WebXR"
+        const xr = await scene.createDefaultXRExperienceAsync({ floorMeshes: [ground] });
+
+        // XR-Sitzung abrufen
+        const xrSession = xr.baseExperience.sessionManager.session;
+        
+        const targetMesh = nagelPuzzleMoveable; // Hier musst du den Code zum Erstellen oder Laden deines Ziel-Meshs einfügen
+
+        // Vorherigen Status des Controllers speichern
+        let previousPosition: Nullable<Vector3> = null;
+        let previousRotation: Nullable<Quaternion> = null;
+
+        // Controller-Status für "Dragging" speichern
+        let isDragging = false;
+
+        // Event-Handler für den rechten Controller hinzufügen
+        xr.input.onControllerAddedObservable.add((controller) => {
+        // Überprüfen, ob der Controller der rechte Controller ist
+        if (controller.inputSource.handedness === 'right') {
+            // Event-Handler für den Trigger hinzufügen
+            controller.onMotionControllerInitObservable.add(() => {
+            // Event-Handler für den Trigger
+            const triggerComponent = controller.motionController?.getComponent('xr-standard-trigger');
+
+            if (triggerComponent) {
+                triggerComponent.onButtonStateChangedObservable.add((buttonValue) => {
+                if (buttonValue.value > 0.5) {
+                    // Trigger ist gedrückt
+                    isDragging = true;
+                } else {
+                    // Trigger ist nicht gedrückt
+                    isDragging = false;
+                    previousPosition = null;
+                    previousRotation = null;
+                }
+                });
+            }
+            });
+        }
+        });        
+
+
+        // Checke Punkte per Frame
+        scene.onBeforeRenderObservable.add(() => { 
         const currentPunkt = moveableNagelPunkte;
         let distance = 1
         let index = 0
+        // Controller Movement
+        if (isDragging) {
+            // Controller abrufen
+            const controller = xr.input.controllers.find((c) => c.inputSource.handedness === 'right');
+        
+            // Überprüfen, ob der Controller gefunden wurde und die Komponenten vorhanden sind
+            if (controller && controller.grip && controller.grip.position && controller.grip.rotationQuaternion) {
+              // Aktuelle Position und Rotation des Controllers abrufen
+              const currentPosition = controller.grip.position;
+              const currentRotation = controller.grip.rotationQuaternion;
+        
+              // Prüfen, ob die Position und Rotation definiert sind und der vorherige Status vorhanden ist
+              if (currentPosition && currentRotation && previousPosition && previousRotation) {
+                // Skalierung und Rotation des Ziel-Meshes berücksichtigen
+                const scaledPositionDelta = currentPosition.subtract(previousPosition).divide(targetMesh.scaling);
+                const scaledRotationDelta = currentRotation.multiply(previousRotation.conjugate());
+        
+                // Kombinieren der Rotation mit der geänderten Rotation des Ziel-Meshs
+                const targetRotation = targetMesh.rotationQuaternion!.multiply(scaledRotationDelta);
+        
+                // Ziel-Mesh transformieren
+                targetMesh.position.addInPlace(scaledPositionDelta);
+                targetMesh.rotationQuaternion = targetRotation;
+              }
+        
+              // Vorherigen Status aktualisieren
+              previousPosition = currentPosition.clone();
+              previousRotation = currentRotation.clone();
+            }
+          }
+        
+        // Controller Movement Ende
+
         for (let i = 0; i < moveableNagelPunkte.length; i++) {
             // Schaue ob wir eine Kollision haben, und wenn ja, ob diese Tiefer drinne ist als unsere bereits gespeicherte
             
