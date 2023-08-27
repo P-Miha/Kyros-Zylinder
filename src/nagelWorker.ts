2d0de81aeec1e0699d1ee31289a3ca70fd1627a9
P-Miha <mplisko@students.uni-mainz.de>
2023-08-27 17:59:28 +0200

    Implemented Worker, performance is worse.
    Perhaps Babylon cannot perform its optimisations on the workerthread?

--- src/nagelWorker.ts
+++ src/nagelWorker.ts
@@ -1,21 +1,20 @@
-import { Matrix, Mesh, Vector3} from "@babylonjs/core";
+import { Matrix, Mesh, Vector3, float, int} from "@babylonjs/core";
 import { SDFData, loadSDFFile, parseSDFFileContent} from "./sdfParser";
 import { distanceToWorldpoint } from "./nagelDistanceField";
 import { loadOffFile, parseOffFileContent } from "./offParser";
-
-let sdfData: SDFData
-let meshData: Mesh
-let pointsData: Vector3
+import { ccDelta, qqDelta } from "./contactForce";
 
 
 // Worker-Thread
 /**
  * Nagel-Worker für die Kollisionserkennung
- * Erhält die Worldmatrix des Meshes und die Punkte des Meshes
+ * Erhält Worldmatrix, Worldposition und oriantation des Meshes
+ * Zieht Punkte und SDF von Internet bei erstellung des Workers
  * 
- * Gibt die Distanz des Punktes zur SDF zurück, 
+ * Berechnet die Distanz der Punkte zur SDF, und schickt gegebenfalls Delta-Werte zurück
+ *  
  * @param position
- * @returns Distanz des Punktes zur SDF
+ * @returns Delta-Werte
  */
 
 // Setup and getting Files
@@ -30,15 +29,30 @@ const sdfContent = parseSDFFileContent(await loadFile);
 
 //Listener für die Nachrichten
 self.addEventListener("message", (event) => {
-    // Kriege Mesh-Worldmatrix als Matrix
+    // Kriege Input in Form:
+    // [MeshWorldMatrix, WorldPosition, Orientation]
     const meshWorldMatrix = event.data[0];
+    const worldPosition = event.data[1];
+    const orientation = event.data[2];
+    
     const newPositions = CalculatePoints(offInfo.vertices, meshWorldMatrix);
-    const distance = CalculateDistance(newPositions, sdfContent);
+    const distanceResult = CalculateDistance(newPositions, sdfContent);
+    const distance = distanceResult[0];
+    const index = distanceResult[1];
+
+    //Update Normalenvektor
+    const normalVector = new Vector3(offInfo.normals[index].x, offInfo.normals[index].y, offInfo.normals[index].z);
+    let transformedNormal = Vector3.TransformNormal(normalVector, meshWorldMatrix);
+    transformedNormal = transformedNormal.multiply(new Vector3(1, 1, 1));
+
     if (distance < 0.1) {
-        self.postMessage(distance);
-    }
-    else {
-        self.postMessage(1);
+        //self.postMessage(distance, index);
+        // Distance kleiner gleich 0, daher Kollision
+        // Berechne Delta
+        const positionDelta = ccDelta(distance, sdfContent.bbox.min, sdfContent.bbox.max, newPositions[index], transformedNormal, worldPosition);
+        const orientationDelta = qqDelta(distance, sdfContent.bbox.min, sdfContent.bbox.max, newPositions[index], worldPosition, transformedNormal, orientation);
+        // Send Info back to Main Thread
+        self.postMessage([positionDelta, orientationDelta]);
     }
 });
 
@@ -53,13 +67,17 @@ function CalculatePoints(points: Vector3[], meshWorldMatrix: Matrix) {
     return newPositions;
 }
 // Berechne die Distanz des Punktes zur SDF um zu prüfen ob Kollisionen vorliegen
-function CalculateDistance(points: Vector3[], sdfData: SDFData): number {
+function CalculateDistance(points: Vector3[], sdfData: SDFData) {
     let lowestDistance = 1;
+    let pointIndex = 0;
     points.forEach((point) => {
         const distance = distanceToWorldpoint(point, sdfData);
         if (distance < lowestDistance) {
             lowestDistance = distance;
+            pointIndex = points.indexOf(point);
         }
-    })
-    return lowestDistance;
+    });
+
+    // return Distance , Index
+    return [lowestDistance, pointIndex];
 }
